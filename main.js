055e9a4907c99c5d2a5c5f2e5b0594581480ad4b
Agape-Justice <138728416+Agape-Justice@users.noreply.github.com>
2024-09-16 22:32:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let imageSrc = image.src;
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-
         navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('Service worker registered:', registration);
@@ -19,43 +18,46 @@ if ('serviceWorker' in navigator) {
             });
     })
 }
-
-generateBtn.addEventListener('click', e => {
-    let writeBoxValue = writeBox.value;
-    e.preventDefault();
-    if (writeBox.value === "") {
-        alertBox.style.display = 'block';
-        image.style.display = 'none';
-        setTimeout(() => {
-            alertBox.style.display = 'none';
-        }, 1000)
-    }
-
-    else {
-        if (navigator.onLine) {
-            async function fetchData() {
-                let apiString = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${writeBoxValue}`;
-                let response = await fetch(apiString);
-                let data = URL.createObjectURL(await response.blob())
-                image.src = data;
-                image.style.display = 'none';
-                load.style.display = 'block';
-                setTimeout(() => {
-                    load.style.display = 'none';
-                    image.style.display = 'block';
-                }, 1000)
-            }
-            fetchData()
-        }
-
-        else {
-            alertBox.innerHTML = "You're Offline";
+function work() {
+    generateBtn.addEventListener('click', e => {
+        let writeBoxValue = writeBox.value;
+        e.preventDefault();
+        if (writeBox.value === "") {
             alertBox.style.display = 'block';
+            image.style.display = 'none';
             setTimeout(() => {
                 alertBox.style.display = 'none';
             }, 1000)
         }
-    }
 
-})
+        else {
+            if (navigator.onLine) {
+                async function fetchData() {
+                    let apiString = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${writeBoxValue}`;
+                    let response = await fetch(apiString);
+                    let data = URL.createObjectURL(await response.blob())
+                    image.src = data;
+                    image.style.display = 'none';
+                    load.style.display = 'block';
+                    setTimeout(() => {
+                        load.style.display = 'none';
+                        image.style.display = 'block';
+                    }, 1000)
+                }
+                fetchData()
+            }
 
+            else {
+                alertBox.innerHTML = "You're Offline";
+                alertBox.style.display = 'block';
+                setTimeout(() => {
+                    alertBox.style.display = 'none';
+                }, 1000)
+            }
+        }
+
+    })
+
+}
+
+work ();
